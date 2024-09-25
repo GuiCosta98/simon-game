551a28c56792d787e1174763b3dd17756bf41884
GuiCosta98 <guui.coosta@gmail.com>
2024-09-25 18:14:58 -0300

    Correções na parte lógica de algumas funções e adição da função de inicialização do jogo por meio do teclado.

--- game.js
+++ game.js
@@ -1,43 +1,72 @@
 var gamePattern = [];
 var userClickedPattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
+var pressedKey = true;
+var level = 0;
 
-function animatePress(currentColour){
+function playSound(nameSound) {
 
-    $("#" + gamePattern).addClass(".pressed");
-
-
-}
-
-function playSound(name) {
-
-    var audioFile = new Audio ("./sounds/" + gamePattern + ".mp3");
+    var audioFile = new Audio ("./sounds/" + nameSound + ".mp3");
     audioFile.play();
+};
 
-}
+function animatePress(currentColour) {
+
+    setTimeout(function() { 
+
+        $("#" + currentColour).removeClass("pressed");
+
+    }, 100);
+
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(100);
+    
+};
 
 function nextSequence() {
 
     var randomChosenColour; 
     var randomNumber = Math.floor(Math.random() * 4); 
+    level++;
+
+    $("h1").text('level ' + level);
 
     randomChosenColour = buttonColours [randomNumber];
 
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+
     gamePattern.push(randomChosenColour);
 
+};
 
-}
+$(".btn").on("click", function(){
 
-nextSequence();
+    var userChosenColour = $(this).attr("id");
+    
+    
+    animatePress(userChosenColour);
+    playSound(userChosenColour);
 
-$("#" + gamePattern).on("click", function(){
-
-    var userChosenColour = $("#" + gamePattern);
-    $("#" + gamePattern).fadeOut(100).fadeIn(100);
-    animatePress($("#".gamePattern));
-    playSound($("#".gamePattern));
     userClickedPattern.push(userChosenColour);
-
+    
+    console.log(userClickedPattern);
+    console.log(userChosenColour);
+  
 
 });
 
+$("body").on("keydown", function() {
+
+    if (pressedKey == true) {
+
+        $("h1").text("Level 0");
+        nextSequence();
+        
+        pressedKey = false;
+
+    }
+
+});
